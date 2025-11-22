42c2fe596b7942bc613bcccd566b392bb9daf756
Zion <zioncmulangala@gmail.com>
2025-11-22 07:10:49 +0200

    added homepage and auth test

---
+++ script.js
@@ -0,0 +1,15 @@
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function testAuth() {
+    const { data, error } = await client.auth.signInWithPassword({
+        email: "test@example.com",
+        password: "password123"
+    });
+
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+}
+
+testAuth();
