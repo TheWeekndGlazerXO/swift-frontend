cd7d4f6adae20a52e9eebc761b0ec161526aacd5
Zion <zioncmulangala@gmail.com>
2025-11-22 12:02:24 +0200

    fixed supabase import

--- config.js
+++ config.js
@@ -4,3 +4,6 @@ export const supabase = createClient(
   "https://jduumpdgipvanpylnbdb.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTY5ODc1MzYyMywiZXhwIjoyMDE1MTkzNjIzfQ.qGrncgL2vM5weWxuYmRiiwcmSmFZPwjcDFSPpbSJBfQ"
 );
+
+
+
